f0ac7797706deccbc90e8f965f1f865256abef3d
carlos <carlosayanesp@gmail.com>
2023-09-06 16:32:57 -0300

    se agrega archivo gitignore basico

---
+++ src/main/java/com/icaro/clase2/A1HolaMundo.java
@@ -0,0 +1,19 @@
+package com.icaro.clase2;
+
+public class A1HolaMundo {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo!"); // Imprime String
+        System.out.println(10); // Imprime un entero
+        System.out.println(20.56); // Imprime un flotante
+        System.out.println(true); // Imprime un booleano
+        System.out.println(false);
+        System.out.println('c'); // Imprime un caracter
+
+        //Concatenacion
+        System.out.println("Hola " + "mundo, hoy es: " + 31);
+
+    }
+}
---
+++ src/main/java/com/icaro/clase2/A2ImprimirSemana.java
@@ -0,0 +1,14 @@
+package com.icaro.clase2;
+
+public class A2ImprimirSemana {
+
+    public static void main(String[] args) {
+        System.out.println("Dia 1 - Lunes");
+        System.out.println("Día 2 - Martes");
+        System.out.println("Día 3 - Miércoles");
+        System.out.println("Día 4 - Jueves");
+        System.out.println("Día 5 - Viernes");
+        System.out.println("Día 6 - Sábado");
+        System.out.println("Día 7 - Domingo");
+    }
+}
